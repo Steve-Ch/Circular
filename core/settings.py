5c739b934eeb0973412ef1ccd83b8a9b509707a7
steve <stevechris179@gmail.com>
2026-03-09 06:09:36 +0100

    logo issue fix

--- core/settings.py
+++ core/settings.py
@@ -230,10 +230,10 @@ JAZZMIN_SETTINGS = {
     "show_ui_builder" : False,
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "logo.jpeg",
+    "site_logo": "logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": "logo.jpeg",
+    "login_logo": "logo_dark.png",
 
     # Logo to use for login form in dark themes (defaults to login_logo)
     # "login_logo_dark": None,
